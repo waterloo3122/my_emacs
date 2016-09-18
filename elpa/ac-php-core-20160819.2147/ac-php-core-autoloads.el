81b6fecaa0cc578f1cc4073651cea10d7ae10f16
pp <waterloopan@qq.com>
2016-09-18 09:21:03 +0800

    add fix

--- elpa/ac-php-core-20160819.2147/ac-php-core-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; ac-php-core-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "ac-php-comm-tags-data" "ac-php-comm-tags-data.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ac-php-comm-tags-data.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ac-php-comm-tags-data" '("ac-php-comm-tags-data-list")))
-
-;;;***
-
-;;;### (autoloads nil "ac-php-core" "ac-php-core.el" (0 0 0 0))
-;;; Generated autoloads from ac-php-core.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ac-php-core" '("ac-php-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("ac-php-core-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ac-php-core-autoloads.el ends here
